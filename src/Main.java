633925944add3e0e441dbe997be954440484ef33
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2021-12-15 18:50:17 +0100

    ćw1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.print("print ");
+        System.out.println("println");
+        System.out.printf("printf\ttab\n\nnowa linia");
     }
 }

eba9d2a1a2e90857ac40f1ba787288f348384024
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2021-12-15 20:25:07 +0100

    ćw5

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,31 @@ public class Main {
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
 
+        if (21 % 3 == 0) {
+            System.out.println("Podzielny przez 3");
+        } else {
+            System.out.println("Niepodzielny przez 3");
+        }
+
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
+        System.out.println("Numer albumu " + (57647 % 2 == 0 ? "jest parzysty" : "jest nieparzysty"));
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
-
+        System.out.println("Podaj liczbę:");
+        Scanner scan = new Scanner(System.in);
+        double liczba = Double.parseDouble(scan.next());
+        if (liczba < 0) {
+            System.out.println("Liczba jest mniejsza od 0");
+        } else if (liczba == 3.14) {
+            System.out.println("Podano liczbę Pi");
+        } else if (liczba >= 10) {
+            System.out.println("Liczba jest większa lub równa 10");
+        } else {
+            System.out.println("Podano liczbę: " + liczba);
+        }
     }
 }

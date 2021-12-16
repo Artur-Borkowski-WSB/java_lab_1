aa2c2930e926ebe4360da2f0e147a3555a965741
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2021-12-16 20:09:42 +0100

    ćw6

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        /* Tablice - cw_6 */
+		/* Tablice - cw_6 */
 
-        /* zad1. stworzyć tablicę typu double 5-cio elementową,
-        przypisać do niej wartości i wyświetlić */
+		/* zad1. stworzyć tablicę typu double 5-cio elementową,
+		przypisać do niej wartości i wyświetlić */
 
-        double[] tab = new double[5];
-        tab[0] = 3.14;
-        tab[1] = 2.5;
-        tab[2] = 7.5;
-        tab[3] = 1.2345;
-        tab[4] = 5;
-        for (int i = 0; i < tab.length; i++) {
-            System.out.println(tab[i]);
-        }
-    }
+		double[] tab = new double[5];
+		tab[0] = 3.14;
+		tab[1] = 2.5;
+		tab[2] = 7.5;
+		tab[3] = 1.2345;
+		tab[4] = 5;
+		for (double el : tab) {
+			System.out.println(el);
+		}
+	}
 }

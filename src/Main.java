065c7ffcebb02b588b19b7a3e95dbe3de70a6547
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2021-12-16 19:53:55 +0100

    ćw2

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,48 @@
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
-        /* ćw.2 */
+		int zmiennaInt = 7;
+		System.out.println("zmienna int = " + zmiennaInt);
+		/* ćw.2 */
 
-        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+		/*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
 
-        int liczba = 1234;
-        double liczba2 = 12.34;
-        String teskt = "Lorem ipsum dolor";
-        boolean bool = true;
-        System.out.println("zad1");
-        System.out.println("zmienna int = " + liczba);
-        System.out.println("zmienna double = " + liczba2);
-        System.out.println("zmienna String = " + teskt);
-        System.out.println("zmienna boolean = " + bool);
+		int liczba = 1234;
+		double liczba2 = 12.34;
+		String teskt = "Lorem ipsum dolor";
+		boolean bool = true;
+		char literka = 'X';
+		System.out.println("zad1");
+		System.out.println("zmienna int = " + liczba);
+		System.out.println("zmienna double = " + liczba2);
+		System.out.println("zmienna String = " + teskt);
+		System.out.println("zmienna boolean = " + bool);
+		System.out.println("zmienna char = " + literka);
 
-        /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+		/*
+		 *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+		 *    mnożenia, dziealenia i modulo na zmiennych:
+		 *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+		 *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+		 *  */
 
-        int liczbaA = 39;
-        int liczbaB = 11;
-        double liczbaX = 3.14;
-        double liczbaZ = 1.5;
+		int liczbaA = 39;
+		int liczbaB = 11;
+		double liczbaX = 3.14;
+		double liczbaZ = 1.5;
 
-        System.out.println("zad2\na)");
-        System.out.println(liczbaA + liczbaB);
-        System.out.println(liczbaA - liczbaB);
-        System.out.println(liczbaA * liczbaB);
-        System.out.println(liczbaA / liczbaB);
-        System.out.println(liczbaA % liczbaB);
-        System.out.printf("\nb)\n");
-        System.out.println(liczbaX + liczbaZ);
-        System.out.println(liczbaX - liczbaZ);
-        System.out.println(liczbaX * liczbaZ);
-        System.out.println(liczbaX / liczbaZ);
-        System.out.println(liczbaX % liczbaZ);
-    }
+		System.out.println("zad2\na)");
+		System.out.println(liczbaA + liczbaB);
+		System.out.println(liczbaA - liczbaB);
+		System.out.println(liczbaA * liczbaB);
+		System.out.println(liczbaA / liczbaB);
+		System.out.println(liczbaA % liczbaB);
+		System.out.println("b)");
+		System.out.println(liczbaX + liczbaZ);
+		System.out.println(liczbaX - liczbaZ);
+		System.out.println(liczbaX * liczbaZ);
+		System.out.println(liczbaX / liczbaZ);
+		System.out.println(liczbaX % liczbaZ);
+	}
 }
